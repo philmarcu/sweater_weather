3364f3c2ade2b45c243ffca1a5b52cffae6b4801
philmarcu <philmark18@gmail.com>
2022-09-26 14:47:30 -0600

    Add final expect for response / parsed comparison book request

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Books Request', :vcr do
     location = "Denver,CO"    
     qty = 5
     parsed = JSON.parse(File.read('spec/fixtures/books_call.json'), symbolize_names: true)
-
+  
     get "/api/v1/book-search?location=#{location}&quantity=#{qty}"
 
     attributes = parsed[:data][:attributes]
@@ -13,6 +13,7 @@ RSpec.describe 'Books Request', :vcr do
     books = parsed[:data][:attributes][:books]
     
     expect(parsed).to be_a Hash
+    expect(parsed.to_json.size).to eq(response.body.size)
     expect(parsed[:data]).to be_a Hash
     expect(type).to eq("books")
     expect(books.size).to eq(5)
