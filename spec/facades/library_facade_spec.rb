b52ad78003f6e7ee05161751f1f1031bc12e85a7
philmarcu <philmark18@gmail.com>
2022-09-26 11:13:32 -0600

    Refactor facade test for library

--- spec/facades/library_facade_spec.rb
+++ spec/facades/library_facade_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe 'LibraryFacade', :vcr do
   describe '#get_books' do
     it 'formats the books found by library service' do
       books = LibraryFacade.get_books("denver")
+      book = books.first
 
       expect(books).to be_a Array
+      expect(book.title).to be_a String
+      expect(book.publisher).to be_a String
+      expect(book.olid).to be_a String
+      expect(book.isbn).to be_a String
     end
 
     it 'can get publishers' do
