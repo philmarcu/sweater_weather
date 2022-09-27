ff77964c03efe6e7682c6ea82efb5af05394b2af
philmarcu <philmark18@gmail.com>
2022-09-27 15:27:18 -0600

    Add roadtrip method to mapquest service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -3,4 +3,9 @@ class MapQuestService
     response = BaseService.mapquest_conn.get("geocoding/v1/address?location=#{location}")
     BaseService.get_json(response)
   end
+
+  def self.roadtrip(start, endpoint)
+    response = BaseService.mapquest_conn.get("directions/v2/route?from=#{start}&to=#{endpoint}")
+    BaseService.get_json(response)
+  end
 end
