bf94e8ccb26579fffc1cd2d431ad94b6a65b6a2b
philmarcu <philmark18@gmail.com>
2022-09-27 12:54:02 -0600

    Save changes

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,16 +1,4 @@
 module ExceptionHandler
-  extend ActiveSupport::Concern
-
-  # included do
-  #   rescue_from ActiveRecord::RecordNotFound do |e|
-  #     json_response({ message: e.message }, :not_found)
-  #   end
-
-  #   rescue_from ActiveRecord::RecordInvalid do |e|
-  #     json_response({ message: e.message }, :unprocessable_entity)
-  #   end
-  # end
-
   def json_error(user)
     render json: { status: 400, message: "#{user.errors.full_messages.join(" , ")}", data:{} }, status: :bad_request 
   end
